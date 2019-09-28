f2f7aa8ff52498c0235e32cdf06470abe167738d
zumiko <50863797+zumiko@users.noreply.github.com>
2019-09-28 15:42:35 -0700

    this one can iterate through pages
    Still need to make it so it works for more pages

--- scrapeprelimw3.py
+++ scrapeprelimw3.py
@@ -7,9 +7,12 @@ import json
 # Import the CrawlerProcess: for running the spider
 from scrapy.crawler import CrawlerProcess
 
+
+
 # Create the Spider class
 class gfm_Spider(scrapy.Spider):
   name = "gfm_spider"
+  i = 1
 
   custom_settings = {
     'USER_AGENT' : 'gfm_spider',
@@ -19,21 +22,28 @@ class gfm_Spider(scrapy.Spider):
   # start_requests method
   def start_requests(self):
     allowed_domains = ['gofundme.com']
-    url1 = "https://www.gofundme.com/mvc.php?route=categorypages/load_more&page=2&term=&cid=11"
-    print("initiating requests")
-    yield scrapy.Request(url = url1,
-                         callback = self.parse_front)
+    url1 = "https://www.gofundme.com/mvc.php?route=categorypages/load_more&page=1&term=&cid=11"
+    while self.i <= 9: 
+      url1 = list(url1)
+      url1[69] = str(self.i)
+      url1 = "".join(url1)    
+      print("initiating request" + str(self.i))
+      yield scrapy.Request(url = url1,
+                           callback = self.parse_front)
+      self.i += 1 
   # First parsing method
   def parse_front(self, response):
+    #print("this is parse number " + str(self.i))
     tile = response.css('div.react-campaign-tile') #this is getting each tile 
     tile_links = tile.xpath('./a/@href') #this gets links to individual campaigns 
     links_to_follow = tile_links.extract()
     print(links_to_follow)
 
 
-# Run the Spider
+
+#running spyder
 process = CrawlerProcess()
 process.crawl(gfm_Spider)
 process.start()
 
-# Print a preview of courses
+
